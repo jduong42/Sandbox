64478c43006676a3112c1ee26330ddf254bc878c
Juha Duong <juha.duong@edu.turkuamk.fi>
2025-01-28 12:36:03 +0200

    Added NavBar, but not called in the app yet. Finished Lesson 1 - NextJS Basic App Setup

---
+++ match-me/src/app/messages/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function MessagesPage() {
+  return (
+    <div>
+      <h3 className="text-3xl">This will be the messages page.</h3>
+    </div>
+  );
+}
