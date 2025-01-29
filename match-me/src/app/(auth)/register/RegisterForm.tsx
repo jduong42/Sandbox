3a28b9c03a6121eb7d906d6dadcdde2053629230
Juha Duong <juha.duong@edu.turkuamk.fi>
2025-01-29 13:42:56 +0200

    Installed prettier and fixed vertical-center problem with forms.

--- match-me/src/app/(auth)/register/RegisterForm.tsx
+++ match-me/src/app/(auth)/register/RegisterForm.tsx
@@ -1,78 +1,77 @@
 "use client";
 
-import {
-    RegisterSchema
-} from "@/lib/schemas/RegisterSchema";
+import { RegisterSchema } from "@/lib/schemas/RegisterSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
-import {
-    Card,
-    CardHeader,
-    CardBody,
-    Button,
-    Input
-} from "@heroui/react";
+import { Card, CardHeader, CardBody, Button, Input } from "@heroui/react";
 import React from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { GiPadlock } from "react-icons/gi";
 
 export default function RegisterForm() {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors, isValid, isSubmitting }
-    } = useForm<RegisterSchema>({
-        resolver: zodResolver(RegisterSchema),
-        mode: "onTouched"
-    });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid, isSubmitting },
+  } = useForm<RegisterSchema>({
+    resolver: zodResolver(RegisterSchema),
+    mode: "onTouched",
+  });
 
-    const onSubmit = (data: RegisterSchema) => {
-        console.log(data);
-    };
+  const onSubmit = (data: RegisterSchema) => {
+    console.log(data);
+  };
 
-    return (
-        <Card className="w-3/5 mx-auto">
-            <CardHeader className="flex flex-col items-center justify-center">
-            </CardHeader>
-            <CardBody>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className="space-y-4">
-                        <Input
-                            defaultValue=""
-                            label="Name"
-                            variant="bordered"
-                            {...register("name")}
-                            isInvalid={!!errors.name}
-                            errorMessage={errors.name?.message as string}
-                        />
-                        <Input
-                            defaultValue=""
-                            label="Email"
-                            variant="bordered"
-                            {...register("email")}
-                            isInvalid={!!errors.email}
-                            errorMessage={errors.email?.message as string}
-                        />
-                        <Input
-                            defaultValue=""
-                            label="Password"
-                            variant="bordered"
-                            type="password"
-                            {...register("password")}
-                            isInvalid={!!errors.password}
-                            errorMessage={errors.password?.message as string}
-                        />
-                        <Button
-                            isLoading={isSubmitting}
-                            isDisabled={!isValid}
-                            fullWidth
-                            color="default"
-                            type="submit"
-                        >
-                            Register
-                        </Button>
-                    </div>
-                </form>
-            </CardBody>
-        </Card>
-    )
+  return (
+    <Card className="w-3/5 mx-auto">
+      <CardHeader className="flex flex-col items-center justify-center">
+        <div className="flex flex-col gap-2 items-center text-default">
+          <div className="flex flex-row items-center gap-3">
+            <GiPadlock size={30} />
+            <h1 className="text-3xl font-semibold">Login</h1>
+          </div>
+          <p className="text-neutral-500">Welcome back to MatchMe!</p>
+        </div>
+      </CardHeader>
+      <CardBody>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="space-y-4">
+            <Input
+              defaultValue=""
+              label="Name"
+              variant="bordered"
+              {...register("name")}
+              isInvalid={!!errors.name}
+              errorMessage={errors.name?.message as string}
+            />
+            <Input
+              defaultValue=""
+              label="Email"
+              variant="bordered"
+              {...register("email")}
+              isInvalid={!!errors.email}
+              errorMessage={errors.email?.message as string}
+            />
+            <Input
+              defaultValue=""
+              label="Password"
+              variant="bordered"
+              type="password"
+              {...register("password")}
+              isInvalid={!!errors.password}
+              errorMessage={errors.password?.message as string}
+            />
+            <Button
+              isLoading={isSubmitting}
+              isDisabled={!isValid}
+              fullWidth
+              color="default"
+              type="submit"
+            >
+              Register
+            </Button>
+          </div>
+        </form>
+      </CardBody>
+    </Card>
+  );
 }
--- match-me/src/app/(auth)/register/page.tsx
+++ match-me/src/app/(auth)/register/page.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import RegisterForm from "./RegisterForm";
 
 export default function RegisterPage() {
-    return (
-        <div className="flex items-center justify-center vertical-center">
-            <RegisterForm/>
-        </div>
-    )
+  return (
+    <div className="flex items-center justify-center vertical-center">
+      <RegisterForm />
+    </div>
+  );
 }
