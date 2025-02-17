ea37a8aa53fb73e7f1fe38020ea9f70e8addc628
Juha Duong <juha.duong@edu.turkuamk.fi>
2025-02-17 15:26:36 +0200

    Added dynamic routes for members.

--- match-me/src/app/actions/memberActions.ts
+++ match-me/src/app/actions/memberActions.ts
@@ -20,3 +20,11 @@ export async function getMembers() {
     throw error;
   }
 }
+
+export async function getMemberByUserId(userId: string) {
+  try {
+    return prisma.member.findUnique({ where: { userId } });
+  } catch (error) {
+    console.log(error);
+  }
+}
